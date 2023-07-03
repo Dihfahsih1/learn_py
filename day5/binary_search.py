fdc8cdf992bed83b56826257f34d1792ca9ff899
Dihfahsih1 <dihfahsihm@gmail.com>
2023-07-03 20:14:28 +0300

    handling the validation of the algorithm

--- day5/binary_search.py
+++ day5/binary_search.py
@@ -1,29 +1,41 @@
-def binary_search(list, element):
+def binary_search(lst, element):
     middle = 0
     start = 0
-    end = len(list)
+    end = len(lst)
     steps = 0
 
-    while(start<=end):
-        print("Step", steps, ":", str(list[start:end+1]))
+    while start <= end:
+        print("Step", steps, ":", str(lst[start:end+1]))
 
-        steps = steps +1
+        steps += 1
         middle = (start + end) // 2
 
-        if element == list[middle]:
+        if element == lst[middle]:
             return middle
-        if element < list[middle]:
-            end = middle -1
+        if element < lst[middle]:
+            end = middle - 1
         else:
             start = middle + 1
+    
     return -1
 
 elements = input("Enter list of numbers to be searched, separated by a comma: ")
-my_list = [int(num) for num in elements.split(',')]
 
-target = int(input("Enter the target number from the list: "))
+# Validate the input list
+try:
+    my_list = [int(num) for num in elements.split(',')]
+except ValueError:
+    print("Invalid input format. Please provide a comma-separated list of integers.")
+    exit()
+
+target_input = input("Enter the target number from the list: ")
+
+# Validate the target number
+try:
+    target = int(target_input)
+except ValueError:
+    print("Invalid target number. Please enter an integer.")
+    exit()
 
 result = binary_search(my_list, target)
 print("Target found at index:", result)
-
-binary_search(my_list, target)
